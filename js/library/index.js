ca407e6873ada20ae143105102ae04ffeaeaf4d2
Jeanne Francis Rivas <vashypies@gmail.com>
2023-06-07 16:46:17 +0800

    Enhanced the Library App

---
+++ js/library/index.js
@@ -0,0 +1,144 @@
+const Book1 = new Book('The Hobbit', 'J.R.R Tolkien', 295)
+const Book2 = new Book('Lord of the Rings', 'J.R.R Tolkien', 500)
+const Book3 = new Book('Middle Earth', 'J.R.R Tolkien', 299)
+const Magazine1 = new Magazine('Timely Earth', 'John Boulevard', 100)
+const Dictionary1 = new Dictionary('Every Word From 2000', 'Steve Morder', 500)
+
+let myLibrary = [];
+myLibrary.push(Book1, Book2, Book3)
+
+displayBooks(myLibrary)
+
+function addBookToLibrary(e){
+    e.preventDefault();
+    
+    let getTitle = document.getElementById('bookTitle')
+    let getAuthor = document.getElementById('bookAuthor')
+    let getPages = document.getElementById('bookPages')
+
+    if (getTitle.value === null || getTitle.value === undefined || getTitle.value === '') return false
+    if (getAuthor.value === null || getAuthor.value === undefined || getAuthor.value === '') return false
+    if (getPages.value === null || getPages.value === undefined || getPages.value === '') return false
+    
+    myLibrary.push(new Book(getTitle.value , getAuthor.value, getPages.value))
+
+    getTitle.value = ''
+    getAuthor.value = ''
+    getPages.value = ''
+
+    reset()
+    displayBooks(myLibrary)
+}
+
+function reset(){
+    var reset = document.querySelectorAll('tbody tr')
+    reset.forEach((items) => {
+        items.remove()
+    })
+}
+
+function deleteBook(index) {
+    let tbRemoved = document.querySelector('tr[data-attribute=\'' +index+ '\']')
+    tbRemoved.remove()
+    myLibrary.splice(index, 1)
+
+    reset()
+    displayBooks(myLibrary)
+}
+
+function markAsRead(index) {
+    let tbHighlighted = document.querySelector('tr[data-attribute=\'' +index+ '\']')
+
+    if (myLibrary[index].hasRead == true) return
+    else myLibrary[index].read()
+    
+    reset()
+    displayBooks(myLibrary)
+}
+
+function displayBooks(library){
+    if (library === null || library === undefined) return
+    library.forEach((items, index) => {
+        // Creates Row
+        let tableRow = document.createElement('tr')
+        let tableData_Title = document.createElement('td'), 
+        tableData_Author = document.createElement('td'), 
+        tableData_Pages = document.createElement('td'),
+        tableData_Status = document.createElement('td'),
+        tableData_Action = document.createElement('td')
+    
+        //Creates Delete Button
+        let tableData_Anchor = document.createElement('input')
+        tableData_Anchor.setAttribute("type", "button")
+        tableData_Anchor.setAttribute("value", "Delete")
+        tableData_Anchor.setAttribute("onclick", "deleteBook("+ index +")")
+        
+        // Creates Read Button
+        let tableData_Read = document.createElement('input')
+        tableData_Read.setAttribute("type", "button")
+        tableData_Read.setAttribute("value", "Read")
+        tableData_Read.setAttribute("onclick", "markAsRead("+ index +")")
+
+        tableData_Title.textContent = items.title
+        tableData_Author.textContent = items.author
+        tableData_Status.textContent = (items.hasRead == false)? "Not Read" : "Read"
+        if (items.hasRead == false) tableData_Status.setAttribute("style", "color: red")
+        else tableData_Status.setAttribute("style", "color: green")
+        tableData_Pages.textContent = items.pages
+
+        tableRow.setAttribute("data-attribute", index)
+        
+        tableData_Action.appendChild(tableData_Anchor)
+        tableData_Action.appendChild(tableData_Read)
+        
+        
+        tableRow.appendChild(tableData_Title)
+        tableRow.appendChild(tableData_Author)
+        tableRow.appendChild(tableData_Pages)
+        tableRow.appendChild(tableData_Status)
+        tableRow.appendChild(tableData_Action)
+        
+        return document.getElementById('table_body').append(tableRow)
+    })
+}
+
+function Book (title, author, pages) {
+    this.title = title,
+    this.author = author,
+    this.pages = pages
+
+    this.hasRead = false
+}
+
+Book.prototype.read = function () {
+    this.hasRead = true
+}
+
+Book.prototype.info = function () {
+    return (this.title + ' by ' + this.author + ', ' + this.pages + ' pages, not read yet')
+}
+
+Book.prototype.throw = function () {
+    return ("You've thrown " + this.title + ", which is a " + this.constructor.name)
+}
+
+function Magazine (title, author, pages) {
+    Book.call(this, title, author, pages)
+    this.isThick = function () {
+        if(this.pages > 200) return "Thick Boi"
+        return "Thin Ass"
+    }
+}
+
+Magazine.prototype = Object.create(Book.prototype)
+
+function Dictionary (title, author, pages) {
+    Magazine.call(this, title, author, pages)
+    // Magazine.call(this)
+}
+
+Dictionary.prototype = Object.create(Book.prototype)
+
+Dictionary.prototype.throw = function () {
+    return ("You can't throw a Dictionary")
+}
