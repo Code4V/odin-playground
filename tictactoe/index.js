060600ee2e5d404765163eaa423836b51b564395
Jeanne Francis Rivas <vashypies@gmail.com>
2023-07-16 21:15:14 +0800

    Added winning conditions
    Added player occupied space and several user functions

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -3,10 +3,15 @@ const play_arena = document.querySelector("#play_arena")
 
 const playerFactory = (playershape) => 
 {
+    const occupied_space = [];
     function getPlayer() { return playershape }
+    function getSpace() { return occupied_space }
+    function insertSpace(space) { occupied_space.push(space) }
 
     return {
-        getPlayer
+        getPlayer,
+        getSpace,
+        insertSpace
     }
 }
 
@@ -14,10 +19,36 @@ const playArena = (playerOne, playerTwo) =>
 {
     
     let play_status = [[0, 1, 2],[3, 4, 5],[6, 7, 8]];
-    const play_areas = document.querySelectorAll(".play_area")
+    const play_areas = document.querySelectorAll(".play_area");
     let currentTurn = 1;
 
-    const update_status = (key, currentPlayer) =>
+    const checkStatus = (player) => {
+
+        const playerSpace = player.getSpace();
+        const winning_combinations = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+        winning_combinations.forEach((element)=>
+        {
+           
+            let filtered = element.filter(innerElement => {
+                if(playerSpace.includes(innerElement))
+                {
+                    return true;
+                }
+                else return false;
+            });
+            
+            if(filtered.length === 3)
+            {
+                console.log(`${player.getPlayer()} WON` )
+                play_areas.forEach(element => {
+                    
+                    element.setAttribute("style", "pointer-events:none")
+                })
+            }
+        })
+    }   
+
+    const updateStatus = (key, currentPlayer) =>
     {
         play_status.forEach((innerElement) => {
  
@@ -40,22 +71,27 @@ const playArena = (playerOne, playerTwo) =>
     play_areas.forEach((element, key)=>{
         element.addEventListener("click", () => {
             try{
-                if(currentTurn === 9) throw "TIE! Game Over"
+                if(currentTurn === 10) throw "TIE! Game Over"
                 if(element.innerHTML === "X" || element.innerHTML === "O") throw "Illegal Move!"
-                console.log(play_status)
                 if(currentTurn % 2)
                 {
+                    playerOne.insertSpace(parseInt(element.innerHTML)-1);
                     element.innerHTML = playerOne.getPlayer();
-                    
-                    update_status(key, element.innerHTML)
+                    updateStatus(key, element.innerHTML);
+                    checkStatus(playerOne)
                     ++currentTurn;
-                    console.log("END")
+                    console.log("END TURN")
+                    // console.log(playerOne.getSpace());
                 }  
                 else
                 {
+                    playerTwo.insertSpace(parseInt(element.innerHTML)-1);
                     element.innerHTML = playerTwo.getPlayer();
-                    update_status(key, element.innerHTML)
+                    updateStatus(key, element.innerHTML);
+                    checkStatus(playerTwo)
                     ++currentTurn;
+                    console.log("END TURN")
+                    // console.log(playerTwo.getSpace())
                 }
 
                 
@@ -70,7 +106,6 @@ const playArena = (playerOne, playerTwo) =>
 
     const getTurn = () => 
     {
-        
         return turn = (currentTurn % 2) ? `${currentTurn} X's Turn` : `${currentTurn} O's Turn`;
     }
 
