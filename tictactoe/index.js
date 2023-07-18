7e0a7764864752f47a92e2c04fab6785e0f0c527
Jeanne Francis Rivas <vashypies@gmail.com>
2023-07-18 20:56:06 +0800

    Added winning tiles highlighting
    Added Restart Button
    
    Added BEM methodology for the CSS styling

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,11 +1,10 @@
-
-const play_arena = document.querySelector("#play_arena")
-
-const playerFactory = (playershape) => 
+const playerFactory = (playershape, playerName) => 
 {
     const occupied_space = [];
+    const player_name = playerName;
     function getPlayer() { return playershape }
     function getSpace() { return occupied_space }
+
     function insertSpace(space) { occupied_space.push(space) }
 
     return {
@@ -15,17 +14,37 @@ const playerFactory = (playershape) =>
     }
 }
 
+var highlightDelay; 
+
+
+
 const playArena = (playerOne, playerTwo) =>
 {
     
-    let play_status = [[0, 1, 2],[3, 4, 5],[6, 7, 8]];
-    const play_areas = document.querySelectorAll(".play_area");
     let currentTurn = 1;
 
-    const checkStatus = (player) => {
+    const getTurn = () => 
+    {
+        return turn = (currentTurn % 2) ? `${currentTurn} X's Turn` : `${currentTurn} O's Turn`;
+    }
 
+    const setStatus = (setter) => { play_status = setter }
+    const getStatus = () => { return play_status = [[0, 1, 2],
+                                                    [3, 4, 5],
+                                                    [6, 7, 8]];}
+
+    const getArena = () => 
+    {
+        const play_areas = document.querySelectorAll(".play__arena-area");
+        return play_areas;
+    }
+
+    const checkStatus = (player) => {
         const playerSpace = player.getSpace();
-        const winning_combinations = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]];
+        const winning_combinations = [[0, 1, 2],[3, 4, 5],
+                                      [6, 7, 8],[0, 3, 6],
+                                      [1, 4, 7],[2, 5, 8],
+                                      [0, 4, 8],[2, 4, 6]];
         winning_combinations.forEach((element)=>
         {
            
@@ -39,84 +58,137 @@ const playArena = (playerOne, playerTwo) =>
             
             if(filtered.length === 3)
             {
-                console.log(`${player.getPlayer()} WON` )
-                play_areas.forEach(element => {
-                    
-                    element.setAttribute("style", "pointer-events:none")
-                })
+                highlightDelay = setInterval(function highlightTiles () {
+                    for (let i = 0 ; i < 3 ; i++)
+                    {
+                        const node = document.getElementById("area"+(filtered[i]+1))
+                        
+                        node.classList.toggle("highlight")
+                        
+                        node.style.animationDelay = (150 * i) + "ms";
+                    }
+                }, 1000);
+
+                finishGame();
             }
         })
     }   
 
     const updateStatus = (key, currentPlayer) =>
     {
-        play_status.forEach((innerElement) => {
+        getStatus().forEach((innerElement) => {
  
-            if(innerElement.includes(key))
+            if(!innerElement.includes(key))
             {
-                innerElement.forEach((iielement, iikey) => 
-                { 
-                    if(iielement === key)
-                    {
-                        innerElement.splice(iikey, 1, currentPlayer)
-                    }
-                })
-                
-                
+                return
             }
+
+            innerElement.forEach((iielement, iikey) => 
+            { 
+                if(iielement === key)
+                {
+                    innerElement.splice(iikey, 1, currentPlayer)
+                }
+            })
             
         })
     }
 
-    play_areas.forEach((element, key)=>{
-        element.addEventListener("click", () => {
-            try{
-                if(currentTurn === 10) throw "TIE! Game Over"
-                if(element.innerHTML === "X" || element.innerHTML === "O") throw "Illegal Move!"
-                if(currentTurn % 2)
-                {
-                    playerOne.insertSpace(parseInt(element.innerHTML)-1);
-                    element.innerHTML = playerOne.getPlayer();
-                    updateStatus(key, element.innerHTML);
-                    checkStatus(playerOne)
-                    ++currentTurn;
-                    console.log("END TURN")
-                    // console.log(playerOne.getSpace());
-                }  
-                else
-                {
-                    playerTwo.insertSpace(parseInt(element.innerHTML)-1);
-                    element.innerHTML = playerTwo.getPlayer();
-                    updateStatus(key, element.innerHTML);
-                    checkStatus(playerTwo)
-                    ++currentTurn;
-                    console.log("END TURN")
-                    // console.log(playerTwo.getSpace())
-                }
-
-                
-            }
-            catch(error)
-            {
-                alert(error)
-            }
-            
-        });
-    });
-
-    const getTurn = () => 
+    const startGame = () => 
     {
-        return turn = (currentTurn % 2) ? `${currentTurn} X's Turn` : `${currentTurn} O's Turn`;
+        
+        const play_areas = getArena()
+        play_areas.forEach((element, key)=>{
+            element.addEventListener("click", () => {
+                try{
+                    if(element.innerHTML === playerOne.getPlayer() || 
+                       element.innerHTML === playerTwo.getPlayer()) throw "Illegal Move!"
+
+                    if(currentTurn % 2)
+                    {
+                        
+                        playerOne.insertSpace(parseInt(key));
+                        element.innerHTML = playerOne.getPlayer();
+                        updateStatus(key, element.innerHTML);
+                        checkStatus(playerOne)
+                        ++currentTurn;
+                    }  
+                    else
+                    {
+                        playerTwo.insertSpace(parseInt(key));
+                        element.innerHTML = playerTwo.getPlayer();
+                        updateStatus(key, element.innerHTML);
+                        checkStatus(playerTwo)
+                        ++currentTurn;
+                    }
+    
+                    
+                }
+                catch(error)
+                {
+                    alert(error)
+                }
+                
+            });
+        });
+    }
+
+    const finishGame = () =>
+    {
+        getArena().forEach(element => {  
+            element.setAttribute("style", "pointer-events:none")
+            element.setAttribute("style", "opacity: 90%")
+        })
+    }
+
+    const restartGame = () =>
+    {
+        getArena().forEach(element => {  
+            element.innerHTML = "";
+            element.removeAttribute("style")
+            element.classList.remove("highlight")
+        });
+
+        currentTurn = 1;
+        playerOne.getSpace().length = 0;
+        playerTwo.getSpace().length = 0;
+
+        setStatus([[0, 1, 2],
+                   [3, 4, 5],
+                   [6, 7, 8]]) ;
+
+        clearInterval(highlightDelay);
     }
 
     return {
-        play_status,
-        getTurn
+        getStatus,
+        getTurn,
+        startGame,
+        restartGame
     }
 }
+
+const generateBoard = (() => 
+{
+    const play_arena = document.querySelector("#play_arena");
+    const CELLS = 9;
+
+    for (let i = 1; i <= CELLS; i++ )
+    {
+        const play_area = document.createElement("div");
+
+        play_area.setAttribute("class", "play__arena-area");
+        play_area.setAttribute("id", "area"+i);
+        // play_area.innerHTML = i;
+        play_arena.appendChild(play_area)
+    }   
+
+    return;
+})();
 
 const CrossPlayer = playerFactory("X");
 const CirclePlayer = playerFactory("O");
 
-const Arena1 = playArena(CrossPlayer, CirclePlayer)
 
+const Arena = playArena(CrossPlayer, CirclePlayer);
+Arena.startGame();
