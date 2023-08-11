4f27f0fed2e8b8743088080b368607808269c315
Jeanne Francis Rivas <vashypies@gmail.com>
2023-08-11 19:42:43 +0800

    Tried adding a navigation

--- store/src/index.js
+++ store/src/index.js
@@ -1,11 +1,13 @@
 import Header from './components/header';
 import Product from './components/product';
 import Menu from './components/menu';
+import Navigation from './components/navigation';
+
 
 const main = document.querySelector("#content");
 
 
-
+main.insertAdjacentElement('beforebegin', Navigation(['Home', 'Menu', 'Contact']))
 
 main.insertAdjacentElement('afterbegin',Header());
 
