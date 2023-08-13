413b2d4f6efcf64f0904cf23409b40d2f903bffe
Jeanne Francis Rivas <vashypies@gmail.com>
2023-08-13 19:17:06 +0800

    Added best-seller component modified the product component
    removed the remaining hardcoded components in the index.html CONT

--- store/src/index.js
+++ store/src/index.js
@@ -2,19 +2,18 @@ import Header from './components/header';
 import Product from './components/product';
 import Menu from './components/menu';
 import Navigation from './components/navigation';
-
+import BestSeller from './components/best-seller';
 
 const main = document.querySelector("#content");
 
 
-main.insertAdjacentElement('beforebegin', Navigation(['Home', 'Menu', 'Contact']))
+// main.insertAdjacentElement('beforebegin', Navigation(['Home', 'Menu', 'Contact']))
 
-main.insertAdjacentElement('afterbegin',Header());
 
 document.addEventListener('DOMContentLoaded', ()=>{
-
-    main.appendChild(Menu('Drinks'))
-
+    main.insertAdjacentElement('afterbegin',Header());
+    main.appendChild(BestSeller())
+    main.appendChild(Menu())
 })
 
 // main.innerHTML = "HELLO"
