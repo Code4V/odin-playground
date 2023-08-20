3b1e465cb3d2082d901e95a45a259d6107355d82
Jeanne Francis Rivas <vashypies@gmail.com>
2023-08-20 21:29:19 +0800

    Temporarily removed the navigation on the header

--- store/src/components/header-navigation.js
+++ store/src/components/header-navigation.js
@@ -4,8 +4,14 @@ const HeaderNavigation = () =>
 {
     const MainNavigation = document.createElement("nav");
     MainNavigation.setAttribute("class", "header-navigation");
-  
-    MainNavigation.appendChild(Navigation(["Home", "Menu", "Contact"]));
+
+    const headerNavigationTitle = document.createElement("p");
+    headerNavigationTitle.setAttribute("class", "header-navigation__logo");
+    headerNavigationTitle.textContent = "VIKINGS";
+
+    
+    // MainNavigation.appendChild(Navigation(["Home", "Menu", "Contact"]));
+    MainNavigation.appendChild(headerNavigationTitle);
     return MainNavigation;
 }
 
