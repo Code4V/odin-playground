e7e3c6d7d5b1553a8f0834a903f487c1efa89867
Jeanne Francis Rivas <vashypies@gmail.com>
2023-08-26 07:52:00 +0800

    Added a contact component

---
+++ store/src/components/contact.js
@@ -0,0 +1,10 @@
+const Contact = () => 
+{
+    const contact = document.createElement('section');
+    contact.setAttribute('class', 'contact');
+
+
+    return contact;
+}
+
+export default Contact;
