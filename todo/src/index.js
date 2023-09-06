d7eda25c6d35281562241b4e47ea6fbb47c6f6ae
Jeanne Francis Rivas <vashypies@gmail.com>
2023-09-06 12:50:24 +0800

    Added a testing logger on main js

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,4 +2,9 @@ import Todo from "./components/todo";
 
 var firstTodo = new Todo('Test', 'Test description', new Date(2023, 10, 23))
 
-console.log(firstTodo.getTodo())
+
+const main = document.querySelector('#content');
+const pre = document.createElement('pre');
+pre.textContent = firstTodo.getTodo();
+
+main.appendChild(pre);
