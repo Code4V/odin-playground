c3e7d32cc0e3fdb1bd37d292bcbeded27dadac14
Jeanne Francis Rivas <vashypies@gmail.com>
2023-09-08 23:48:42 +0800

    Configured the package.json
    Added Sass files
    
    Added test visual

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,12 +6,9 @@ import data from "./data/todo.json";
 var addedTodo = new Todo('Test', 'Test description', new Date(2023, 6, 5));
 
 const main = document.querySelector('#content');
-const pre = document.createElement('pre');
 
 addedTodo.updateProject = "The First Project"
 
-
-pre.textContent = addedTodo.todoDetails;
 
 
 
@@ -23,10 +20,22 @@ addTodo(data, addedTodo.todoDetails)
 // console.log(data)
 
 data.forEach((element, index) => {
-  // console.log(element);
   localStorage.setItem(index, JSON.stringify(element));
 })
 
 
+data.forEach(element => {
+  console.log(element)
+  const pre = document.createElement('pre');
+  pre.textContent = `
+  ${element.title} ${element.description}
+  ${element.dueDate} ${element.createdAt}
+
+  ${element.priority}
+  ${element.project}
+  `;
+  main.appendChild(pre);
+})
+
+
 console.log(JSON.parse(localStorage.getItem(1)));
-main.appendChild(pre);
