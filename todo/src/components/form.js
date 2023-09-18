209ef55a1d056b272a544bd697521d22b133c742
Jeanne Francis Rivas <vashypies@gmail.com>
2023-09-18 16:53:38 +0800

    Removed format import

--- todo/src/components/form.js
+++ todo/src/components/form.js
@@ -2,7 +2,6 @@ import FormInput from './formInput';
 import { addTodo } from '../operations/todoOperations';
 import data from '../data/todo.json';
 import Todo from '../classes/Todo';
-import format  from 'date-fns/format';
 import parseISO from 'date-fns/parseISO';
 
 const Form = () =>
@@ -12,7 +11,7 @@ const Form = () =>
   
   formContainer.append(FormInput("title", "Title").InputField());
   formContainer.appendChild(FormInput("project", "Project").InputField());
-  formContainer.appendChild(FormInput("number", "priority").NumberField());
+  formContainer.appendChild(FormInput("number", "Priority").NumberField());
   formContainer.appendChild(FormInput("description", "Description").TextArea());
   formContainer.appendChild(FormInput("dueDate", "Due Date").DateField());
 
