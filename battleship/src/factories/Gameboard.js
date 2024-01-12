48d2bc19426ed836e380d4c161c22a8b050637fd
Jeanne Francis Rivas <vashypies@gmail.com>
2024-01-13 01:06:22 +0800

    Added vertical placement

--- battleship/src/factories/Gameboard.js
+++ battleship/src/factories/Gameboard.js
@@ -2,9 +2,9 @@ export const Gameboard = (() => {
   const gameBoard = (() => {
     const matrix = [];
 
-    for ( let i = 0 ; i < 10 ; i = i + 1 ) {
+    for (let i = 0; i < 10; i = i + 1) {
       const newRow = [];
-      for ( let j = 0 ; j < 10 ; j = j + 1 ) {
+      for (let j = 0; j < 10; j = j + 1) {
         newRow.push(0)
       }
       matrix.push(newRow)
@@ -14,25 +14,35 @@ export const Gameboard = (() => {
   })();
 
   const receiveAttack = (row, col) => {
-    if(gameBoard[row][col] === 1) return null;
+    if (gameBoard[row][col] === 1) return null;
 
     gameBoard[row][col] = 1;
     return null;
   }
-  
+
   const placeShip = (Ship, row, col, options = { isVertical: false }) => {
     const shipInitial = Ship.getType()[0];
-    let overflowSpot = 1;
+    let stepBackwards = 1;
 
-    for (let i = 0 ; i < Ship.getLength() ; i = i + 1){
-      
-      if(gameBoard[row][col + i] === undefined) {
-        gameBoard[row][col - overflowSpot] = shipInitial;
-        overflowSpot = overflowSpot + 1;
-        continue;
+    for (let i = 0; i < Ship.getLength(); i = i + 1) {
+      if (options.isVertical) {
+        if (gameBoard[row + i] === undefined) {
+          gameBoard[row - stepBackwards][col] = shipInitial;
+          stepBackwards = stepBackwards + 1;
+          continue;
+        }
+
+        gameBoard[row + i][col] = shipInitial;
       }
+      else {
+        if (gameBoard[row][col + i] === undefined) {
+          gameBoard[row][col - stepBackwards] = shipInitial;
+          stepBackwards = stepBackwards + 1;
+          continue;
+        }
 
-      gameBoard[row][col + i] = shipInitial;
+        gameBoard[row][col + i] = shipInitial;
+      }
     }
   }
 
--- battleship/src/index.js
+++ battleship/src/index.js
@@ -1,21 +1,20 @@
-import { Ship } from "./factories/Ship";
+import { Ship } from "./factories/ship";
 import { Gameboard } from "./factories/Gameboard";
 
 const BattleShip = Ship('Carrier');
+const BattleShip2 = Ship('Cruiser');
 
 // BattleShip.hit()
-
-console.log(
-  Gameboard.gameBoard
-)
 
 Gameboard.receiveAttack(1, 1);
 Gameboard.receiveAttack(1, 1);
 Gameboard.receiveAttack(1, 2);
 
 console.log(
-  Gameboard.placeShip(BattleShip, 0, 8),
-  Gameboard.gameBoard,
+  Gameboard.placeShip(BattleShip, 0, 8, {isVertical: true}),
+  Gameboard.placeShip(BattleShip2, 9, 3, {isVertical: true})
 )
+
+console.table(Gameboard.gameBoard)
 
 console.log(BattleShip.getHealth());
