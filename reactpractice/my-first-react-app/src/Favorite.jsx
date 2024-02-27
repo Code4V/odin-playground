a60a8a5bd2f39d107bb96144cdedd5f0152441fc
Jeanne Francis Rivas <vashypies@gmail.com>
2024-02-27 15:34:31 +0800

    Added testing of react States

--- reactpractice/my-first-react-app/src/Favorite.jsx
+++ reactpractice/my-first-react-app/src/Favorite.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
+import { useState } from 'react';
 import Strawberry from './assets/Strawberry.webp';
-import './Favorite.css'
+import './Favorite.css';
+import { v4 as uuid4 } from 'uuid';
 
 // Fruit.propTypes = {
 //   name: PropTypes.string.isRequired,
@@ -10,6 +12,7 @@ import './Favorite.css'
 function Fruit (props) {
   return (
     <li>
+      <p className='favfood__id' data-id={props.id}>{props.id}</p>
       <div className="favfood__img"><img src={Strawberry} alt="" /></div>
       <h1 className="favfood__title">{props.name}</h1>
       <p className="favfood__description">{props.description}</p>
@@ -22,12 +25,35 @@ function Fruit (props) {
 // }
 
 function Favorite (props) {
+
+  const [fruitState, setFruitState] = useState(props.fruits);
+  const [testState, setTestState] = useState([]);
+  // const [numState, setNumState] = useState(0);
+
+  const addFruit = () => {
+    fruitState.push({name: "tesd", description: "kerea", id: uuid4()})
+    console.log(fruitState)
+    setFruitState(fruitState);
+
+    testState.push("fee")
+    setTestState([...testState])
+
+    // setNumState(numState + 1)
+  };
+
   return (
-    <ul className="favfood">
-      {props.fruits.map((fruit, key) => (
-        <Fruit name={fruit.name} description={fruit.description} key={key}/>
-      ))}
-    </ul>
+    <>
+      {testState.map((element) => {
+        return <p>{element}</p>
+
+      })}
+      <ul className="favfood">
+        {/* {fruitState.map((fruit, key) => (
+          <Fruit name={fruit.name} description={fruit.description} key={key} id={fruit.id}/>
+        ))} */}
+      </ul>
+      <button type='button' onClick={addFruit}> Add a Fruit </button>
+    </>
   )
 }
 
--- reactpractice/my-first-react-app/src/main.jsx
+++ reactpractice/my-first-react-app/src/main.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Favorite from './Favorite.jsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { v4 as uuid4 } from 'uuid';
+
 import './index.css'
 
 const fruits = [
   {
     name: "Strawberry",
     description: "Very sweet and tangy",
+    id: uuid4(),
   },
   {
     name: "Banana",
     description: "Long and pleasant to eat",
+    id: uuid4(),
   }
 ]
 
