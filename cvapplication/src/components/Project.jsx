c57cb8afdcb14e8a2ffb1cb657e885f7db2c2a2a
Jeanne Francis Rivas <vashypies@gmail.com>
2024-03-13 23:20:15 +0800

    Added several of my projects

--- cvapplication/src/components/Project.jsx
+++ cvapplication/src/components/Project.jsx
@@ -19,15 +19,16 @@ export class Project extends React.Component {
     return(
       <ProjectContainer>
         <ProjectHeader> Projects Involved </ProjectHeader>
-        <Info header="Lakbay Sa Bayan: Travel Agency Booking System" 
+        <Info header="LAKBAYAN: A BOOKING INFORMATION SYSTEM
+WITH TRAVEL PACKAGE RECOMMENDER" 
               body="These are the projects I've done during my academic career. Additional projects will be added as my professional career continues"
               $colspanstart={1}
               $colspanend={4}/>
-        <Info header="Completed Projects" 
+        <Info header="EIRA UNIVERSITY" 
               body="These are the projects I've done during my academic career. Additional projects will be added as my professional career continues"
               $colspanstart={1}
               $colspanend={4}/>
-        <Info header="Completed Projects" 
+        <Info header="CLINIC MANAGEMENT SYSTEM" 
               body="These are the projects I've done during my academic career. Additional projects will be added as my professional career continues"
               $colspanstart={1}
               $colspanend={4}/>
