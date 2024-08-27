ea6ef3a0fa959b6ef3c8f705313440b26ee1769f
Jeanne Francis Rivas <vashypies@gmail.com>
2024-08-28 02:22:33 +0800

    Added folder creation

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,27 +2,64 @@
 
 const axios = require('axios');
 const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const options = {
-  hostname: 'example.com',
-  port: 443,
-  path: '/todos',
-  method: 'GET',
-}
+const pathName = 'fsDrop/files';
+const fileName = 'fsDrop/files/test.txt';
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  res.on('data', d => {
-    process.stdout.write(d);
-  })
+fs.mkdir(path.join(__dirname, pathName), 
+  { recursive: true}, 
+  (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Directory created successfully');
 });
+// fs.mkdir('fsDrop/file', () => {});
 
-req.on('error', err => {
-  console.error(err);
-});
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, {'Content-Type': 'application/json'});
+//   res.end(JSON.stringify({
+//     data: 'Hello World',
+//   }))
 
-req.end();
+// })
+
+// server.listen(8000);
+
+// const secondServer = http.createServer();
+
+// secondServer.on('request', (req, res) => {
+//   res.writeHead(200, {'Content-Type': 'application/json'});
+//   res.end(JSON.stringify({
+//     data: "This is the second server",
+//   }))
+// })
+
+// secondServer.listen(8001)
+
+// const options = {
+//   hostname: 'example.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'GET',
+// }
+
+// const req = https.request(options, res => {
+//   console.log(`statusCode: ${res.statusCode}`);
+
+//   res.on('data', d => {
+//     process.stdout.write(d);
+//   })
+// });
+
+// req.on('error', err => {
+//   console.error(err);
+// });
+
+// req.end();
 
 
 // axios
