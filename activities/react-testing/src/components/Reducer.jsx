1f8652c1c9642deae235e2181d52f414e40cfae8
Jeanne Francis Rivas <vashypies@gmail.com>
2024-08-27 21:23:59 +0800

    update on reducer

--- activities/react-testing/src/components/Reducer.jsx
+++ activities/react-testing/src/components/Reducer.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 
 export const Reducer = () => {
+  const [ hasError, setHasError ] = React.useState(false);
   function reducer (state, action) {
-    switch (action.type) {
-      case "increment_count": {
-        return { count: state.count + 1 };
-      }
-      case "decrement_count": {
-        return { count: state.count - 1 };
-      }
-      case "set_count": {
-        return { count: action.value };
-      }
-      default: {
-        throw new Error("unkown action: " + action.type);
-      }
+    try {
+      
+      switch (action.type) {
+        case "increment_count": {
+          return { count: state.count + 1 };
+        }
+        case "decrement_count": {
+          return { count: state.count - 1 };
+        }
+        case "set_count": {
+          return { count: action.value };
+        }
+        default: {
+          throw new Error("unkown action: " + action.type);
+        }
+      } 
+    } catch (e) {
+      setHasError(true);
     }
   }
 
@@ -29,11 +35,26 @@ export const Reducer = () => {
     dispatch({type: "decrement_count"});
   }
 
+  function handleError() {
+    dispatch({type: "ASDASDADS"});
+  }
+
+  function removeError() {
+    setHasError(false);
+    dispatch({type: "set_count"})
+  }
+
   return (
     <div>
-      { state.count } <br />
+      { hasError &&
+        <>
+          <p>ERROR BITCH</p>
+        </>
+      }
+      { state && state.count } <br />
       <button onClick={() => handleClick()}>Click Me Please</button><br />
-      <button onClick={() => handleClickBack()}>Click Me Back</button>
+      <button onClick={() => handleError()}>Click Me Back</button>
+      <button onClick={() => removeError()}>Click Me Back</button>
     </div>
   )
 }
